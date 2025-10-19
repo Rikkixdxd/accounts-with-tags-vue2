9319b3582df3462185e0ae60bf25bfc203b4a2ad
Alexander Tkach <atkach@umg-marketing.com>
2025-10-19 22:12:51 +0700

    Убран лишний код и файлы

---
+++ src/components/Accounts.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>asdasdasd</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Accounts',
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/AccountsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <Accounts />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Accounts from '@/components/Accounts.vue'; // @ is an alias to /src
+
+export default Vue.extend({
+  name: 'HomeView',
+  components: {
+    Accounts,
+  },
+});
+</script>
